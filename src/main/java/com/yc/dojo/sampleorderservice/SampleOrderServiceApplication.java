65268f490b841aa64787de3a960d0bd29a1f7b0d
yc-zhang <turalyon.zhangyc@gmail.com>
2018-08-25 21:51:31 +0800

    add sample spring boot.

---
+++ src/main/java/com/yc/dojo/sampleorderservice/SampleOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yc.dojo.sampleorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOrderServiceApplication.class, args);
+	}
+}
