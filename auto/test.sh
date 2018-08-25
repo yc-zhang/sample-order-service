ec0f6717166126e502cc69db7062fdb420d9e51d
yc-zhang <turalyon.zhangyc@gmail.com>
2018-08-25 23:38:38 +0800

    Add agent.

--- auto/test.sh
+++ auto/test.sh
@@ -2,6 +2,6 @@
 
 DOCKER_COMPOSE_CMD="docker-compose --project-name sample-order-service"
 
-${DOCKER_COMPOSE_CMD} run --rm dev gradle clean check
-${DOCKER_COMPOSE_CMD} kill api
-${DOCKER_COMPOSE_CMD} rm -fv api
+${DOCKER_COMPOSE_CMD} run -u root --rm dev gradle clean check
+${DOCKER_COMPOSE_CMD} kill dev
+${DOCKER_COMPOSE_CMD} rm -fv dev
